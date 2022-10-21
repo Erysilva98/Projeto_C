29d223ff94e4180f43e67ecf987f050775be9afd
Erysilva98 <erimilsonsilva98@gmail.com>
2022-10-20 23:30:47 -0300

    Estrutura Pilha ok

--- main.c
+++ main.c
@@ -25,6 +25,25 @@ void empilhar(Pilha *p, int num)
     p->pos++;
 }
 
+// Função POP
+void desempilhar(Pilha *p)
+{
+    Celula *cel = NULL;
+
+    if(p->topo != NULL)
+    {
+        cel = p->topo;
+        p->topo = cel->prox;
+        p->pos--;
+    }
+    else
+    {
+        printf("\n Pilha Vazia \n");
+    }
+}
+
+
+// Função Imprimir Pilha
 void listaPilha(Pilha *p)
 {
     Celula *cel = p->topo;
@@ -48,19 +67,26 @@ void listaPilha(Pilha *p)
 int main()
 {
     int i, op, tam, num;
+    Pilha *p;
+    p->pos = 0;
+    p->topo = NULL;
+    Celula *cel;
 
 	while( 1 ){ 
 
-        printf("\n1- Criar Array Dinamico");
-		printf("\n2- Lista Pilha");
-        printf("\n4- Sair");
-		printf("\n Opcao? ");
+        printf("\n1- Função INIT e PUSH ");
+		printf("\n2- Função para Lista Pilha");
+        printf("\n3- Função POP - Remover e Lista ");   
+        printf("\n4- Função Ordenar e Exibir"); 
+        printf("\n5- Sair");
+		printf("\n\n Opcao? ");
 		scanf("%d", &op);
 
 		switch (op){
 
-            case 1: // Criar 
-                printf("\nFunção INIT \n");
+            // Criar e Adicionar
+            case 1: 
+                printf("\nFunção INIT e PUSH \n");
                 printf("Informe o Tamanho do Vetor: ");
                 scanf("%d", &tam);
 
@@ -68,27 +94,48 @@ int main()
 
                 if(cel)
                 {
-                   printf("\n Memória Alocada com Sucesso \n");
+                   printf("\n Memória Alocada com Sucesso \n\n");
                    for(i = 0; i<tam; i++)
                    { 
                         num = rand() % 100;
+                        printf("%d ",num);
                         empilhar(cel,num);
-                    }                  
+                    }      
+                    printf("\n");            
                 }
                 else
                 {
                     printf("\n Erro de Alocação de Memória \n");
                 }
-
                 break;
 
+            // Exibir Pilha Criada 
             case 2:
             {
+                printf("\nListando a Pilha Criada\n\n");
                 listaPilha(cel);
                 break;
             }
 
-            case 4: // Sair
+            // Remover intem da Pilha
+            case 3:
+            {
+                printf("\nFunção POP - Remover intem do Topo na Pilha\n\n");
+                desempilhar(cel);
+                listaPilha(cel);
+                break;
+            }
+
+            // Ordenação Merge Sort  "Desistir"
+            case 4: 
+            {
+                printf("\nMerge Sort \n");
+                break;
+            }
+
+            // Sair
+            case 5: 
+                printf("\nSaindo ...\n");
                 exit(0);
 
 			default: printf( "\nOPCAO INVALIDA! \n" ); 
@@ -96,6 +143,12 @@ int main()
 	}
     return 0;
 }
+
+
+// Merge Sort
+
+
+
 
 
                 
