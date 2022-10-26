4ed8f4675a9eb1ffbcd4e17f4c57e0f879a351e8
Erysilva98 <erimilsonsilva98@gmail.com>
2022-10-25 22:15:30 -0300

    Teste Falido

--- OK.c
+++ OK.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> 
+#include <string.h>
+#include <locale.h>
 
 #define tam 2
 
@@ -10,7 +11,20 @@ typedef struct
     char *numero; 
 }Registro;
 
-void empilhar(Registro* v, int i)
+typedef struct 
+{
+    int topo;
+    int pos;
+}Pilha;
+
+Pilha *init()
+{
+    Pilha *lista = (Pilha *) malloc(sizeof(Pilha));
+    lista->topo = 0;
+    return lista;
+}
+
+void push(Registro* v, int i)
 {
     v[i].nome = malloc(30);
     v[i].numero = malloc(6);
@@ -20,37 +34,37 @@ void empilhar(Registro* v, int i)
 }
 
 void imprime(Registro* v, int i) {
-    for (int j = 0; j < i; j++) 
-    {
+    for (int j = 0; j < i; j++) {
         printf("Nome: %s | ", v[j].nome);
         printf("Numero: %s\n", v[j].numero);
     }
 }
 
-int main()
+int pilhaCheia(Pilha *p)
 {
-    int i, opc, num;
-    Registro array[tam];
+    return p->topo == tam;
+}
 
-	while( 1 ){ 
+int pilhaVazia(Pilha *p)
+{
+    return p->topo == 0;
+}
 
-        printf("\n1- Função INIT e PUSH ");
-		printf("\n2- Função para Lista Pilha");
-        printf("\n5- Sair");
-		printf("\n\n Opcao? ");
-		scanf("%d", &opc);
-
+int main() {
+    int i = 0, opc;
+    Registro alunos[10];
+    while ( 1 ) 
+    {
+        printf("O que voce deseja ? (1)Inserir (2)Imprimir (3)Sair\n");
+        scanf("%d", &opc);
         switch(opc) 
         {       
             case 1:
-                empilhar(array, i);
+                push(alunos, i);
                 i++;
-                printf("\n"); 
-            
                 break;
             case 2:
-                imprime(array,i);
-                i++;  
+                imprime(alunos, i);
                 break;
             case 3:
                 return 0;
@@ -59,12 +73,3 @@ int main()
     }
     return 0;
 }
-
-
-// Merge Sort
-
-
-
-
-
-                
